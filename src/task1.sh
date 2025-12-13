f7cdec6b89917ffc64c5173f9023dc3f3017ee63
veselov317-ux <veselov317@gmail.com>
2025-12-13 17:04:25 +0200

    task1.sh

--- src/task1.sh
+++ src/task1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-count=$(find /etc -type f 2>/dev/null | wc -l)
-echo "Number of regular files in etc: $count"
+
+TARGET_DIR="/etc"
+FILE_COUNT=0
+
+for item in "$TARGET_DIR"/*; do
+    if [ -f "$item" ] && [ ! -L "$item" ]; then
+        FILE_COUNT=$((FILE_COUNT + 1))
+    fi
+done
+
+echo "Number of regular files in /etc: $FILE_COUNT"
